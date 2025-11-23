d9583751437ec85009219723b9cdf3e6f4175894
ppulcini <paul.pulcini@greenbureau.fr>
2025-11-23 19:16:23 +0100

    wip

--- start.sh
+++ start.sh
@@ -3,16 +3,6 @@
 # Appliquer les migrations
 python manage.py migrate --noinput
 
-python manage.py shell << EOF
-from django.contrib.auth import get_user_model
-User = get_user_model()
-if not User.objects.filter(username="Paul").exists():
-    User.objects.create_superuser("Paul", "pulcinpaul@gmail.com", "P@aulPulc1n1->@-@<-")
-    print("Superuser created")
-else:
-    print("Superuser already exists")
-EOF
-
 # Collecter les fichiers statiques
 python manage.py collectstatic --noinput
 
