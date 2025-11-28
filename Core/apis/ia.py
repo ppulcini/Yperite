e19b989d76e1fa22944380562e1051adba57adbc
ppulcini <paul.pulcini@greenbureau.fr>
2025-11-28 09:07:20 +0100

    try

--- Core/apis/ia.py
+++ Core/apis/ia.py
@@ -1,11 +1,11 @@
 import os
+from django.conf import settings
 from mistralai import Mistral
 import logging
 from .db_Yperite import get_faction_information_global
 model = "mistral-large-latest"
 
-client = Mistral(api_key=os.environ.get("API_KEY_MISTRAL"))
-print("client ia init", os.environ.get("API_KEY_MISTRAL"))
+client = Mistral(api_key=settings.MISTRAL_API_KEY)
 def generate_background(demande):
     content = '''Tu doit crée un background de personnage pour un jeu de rôle a l'aide des info fourni par l'utilisateur mais aussi des les infos de l'univers.
     tu ne donnera pas d'autre information en dehors de la reponse pas d'explication n'y rien d'autre.
