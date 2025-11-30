17fc1749170a81506ff34627881bc8011ef23c40
ppulcini <paul.pulcini@greenbureau.fr>
2025-11-30 19:35:33 +0100

    wip

--- Core/apis/ia.py
+++ Core/apis/ia.py
@@ -8,7 +8,7 @@ model = "mistral-large-latest"
 client = Mistral(api_key=settings.MISTRAL_API_KEY)
 def generate_background(demande):
     content = '''Tu doit crée un background de personnage pour un jeu de rôle a l'aide des info fourni par l'utilisateur mais aussi des les infos de l'univers.
-    tu ne donnera pas d'autre information en dehors de la reponse pas d'explication n'y rien d'autre.
+    Tu devra faire preuves de creativité et d'originalité. tout en gardant la cohérence avec l'univers et les info fournis par l'utilisateur.
     tu proposera une Fiche d’Identité, des Origines, un Parcours de vie, des Motivations et Objectifs. chaque session commencera par ** nom ** exemple  ** Fiche d’Identité **. mais evite apres d'ajouter des étoiles dans le texte.
     Le background doit etre condenser et précis en utilisant 300 mots maximum ( tu ne doit pas afficher ce nombre dans ta reponse ) .
     '''
