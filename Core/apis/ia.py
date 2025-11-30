9d359389a9fd56102b33fb307b020fb3be18d5dc
ppulcini <paul.pulcini@greenbureau.fr>
2025-11-30 20:14:09 +0100

    wip

--- Core/apis/ia.py
+++ Core/apis/ia.py
@@ -7,12 +7,17 @@ model = "mistral-large-latest"
 
 client = Mistral(api_key=settings.MISTRAL_API_KEY)
 def generate_background(demande):
-    content = '''Tu doit crée un background de personnage pour un jeu de rôle a l'aide des info fourni par l'utilisateur mais aussi des les infos de l'univers.
-    Tu devra faire preuves de creativité et d'originalité. tout en gardant la cohérence avec l'univers et les info fournis par l'utilisateur.
-    tu proposera une Fiche d’Identité, des Origines, un Parcours de vie, des Motivations et Objectifs. chaque session commencera par ** nom ** exemple  ** Fiche d’Identité **. mais evite apres d'ajouter des étoiles dans le texte.
-    Le background doit etre condenser et précis en utilisant 300 mots maximum ( tu ne doit pas afficher ce nombre dans ta reponse ) .
+    content =  '''Crée un background **unique et varié** pour un personnage de l'Aéropole.
+- **Évite les clichés** : Les parents ne sont pas toujours ouvriers ou soldats. Explore aussi mais pas forcement toujours des métiers rares (artisans spécialisés, scientifiques dissidents, membres du Culte, contrebandiers, etc.). Tout en gardant une coherence quand meme avec le metiers ou sttaus fournir par l'utilisateur.
+- **Diversifie les origines** : Le personnage peut venir des bas-fonds, mais aussi des niveaux supérieurs, ou avoir un passé hybride.
+- **Inclure des contradictions** : Un enfant de militaire peut détester l'armée, un mutant peut être intégré dans les niveaux supérieurs, etc.
+- **Professions des parents** : Liste au moins 3 options possibles (ex: médecin, ingénieur, artiste clandestin) et choisis-en une au hasard.
+- **Parcours de vie** : Ajoute un événement inattendu (ex: une rencontre avec un mutant, une mission secrète ratée, une découverte interdite).
+- **Tout les infos** fournis sont des exemples de diversité mais n'oublie pas de garder une cohérence avec l'univers de l'Aéropole. Et quecertains personnages peuvent avoir des backgournd avec des passé plus banal ou classique (ex: enfant d'ouvrier devenu artisan, enfant de fonctionnaire devenu policier, etc.)
+     les Infos qui suivent elles sont factuelles et doivent être respectées pour garder la cohérence de l'univers et de la demande de l'utilisateur
+     - Voici les info de l'univers: ''' + get_faction_information_global() + '''\n
+     - Voici les info fournis par l'utilisateur  : '''+ demande + '''\n
     '''
-    content += content + "\nVoici les info fournis par l'utilisateur  : " + demande + "\n voici les info de l'univers: " + get_faction_information_global()
     try:
         chat_response = client.chat.complete(
             model= model,
